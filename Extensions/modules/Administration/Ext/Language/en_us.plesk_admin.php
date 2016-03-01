5b6b6de8a26f3b751cc7a266bf5fab5365be96d3
Glenn Hermans <glhermans@gmail.com>
2016-03-01 01:39:59 +0100

    Removed duplicate language file

--- Extensions/modules/Administration/Ext/Language/en_us.plesk_admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    $mod_strings['LBL_PLESK_ADMIN_LINK2_TITLE'] = 'Link Name';
-    $mod_strings['LBL_PLESK_ADMIN_LINK2_DESCRIPTION'] = 'Link Description';
-    $mod_strings['LBL_SECTION_HEADER'] = 'Section Header';
-    $mod_strings['LBL_SECTION_DESCRIPTION'] = 'Section Description';
