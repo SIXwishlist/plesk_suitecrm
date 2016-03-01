5b20a07cb853a35bf8b1135cb371d574e7f134d9
Glenn Hermans <glhermans@gmail.com>
2016-03-01 01:29:03 +0100

    Started working on plesk integration for SuiteCRM

---
+++ Extensions/modules/Administration/Ext/Language/en_us.plesk_admin.php
@@ -0,0 +1,6 @@
+<?php
+
+    $mod_strings['LBL_PLESK_ADMIN_LINK2_TITLE'] = 'Link Name';
+    $mod_strings['LBL_PLESK_ADMIN_LINK2_DESCRIPTION'] = 'Link Description';
+    $mod_strings['LBL_SECTION_HEADER'] = 'Section Header';
+    $mod_strings['LBL_SECTION_DESCRIPTION'] = 'Section Description';
