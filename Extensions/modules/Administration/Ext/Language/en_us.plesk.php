1bb055f9faadad661ab7c9c262cf1d70610ac696
Glenn Hermans <glhermans@gmail.com>
2016-03-01 02:07:14 +0100

    Moved files to custom dir

--- Extensions/modules/Administration/Ext/Language/en_us.plesk.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    $mod_strings['LBL_LINK_NAME'] = 'Link Name';
-    $mod_strings['LBL_LINK_DESCRIPTION'] = 'Link Description';
-    $mod_strings['LBL_SECTION_HEADER'] = 'Plesk API';
-    $mod_strings['LBL_SECTION_DESCRIPTION'] = 'Section Description';
